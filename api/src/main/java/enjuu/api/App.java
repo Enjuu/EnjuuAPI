348dc2597d3fa8f7accf54bdbb585b0b9fd254e5
MarcPlaying <herpersmarcandre3@gmail.com>
2018-10-17 14:46:39 +0200

    fixed mysql connection

--- api/src/main/java/enjuu/api/App.java
+++ api/src/main/java/enjuu/api/App.java
@@ -2,31 +2,43 @@ package enjuu.api;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.HashMap;
+
 import org.json.simple.parser.ParseException;
+
 
 import spark.Request;
 import spark.Response;
 import spark.Route;
 import spark.Spark;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
+	
+	public static Connection connection = null;
+	
 	/**
 	 * Main void for starting the api
 	 * 
 	 * @throws ParseException 
 	 * @throws IOException 
 	 * @throws FileNotFoundException 
+	 * @throws SQLException 
+	 * @throws ClassNotFoundException 
 	 */
-    public static void main( String[] args ) throws FileNotFoundException, IOException, ParseException
+	
+	static Connection crunchifyConn = null;
+	
+    public static void main( String[] args ) throws FileNotFoundException, IOException, ParseException, ClassNotFoundException
     {
         Config.createConfig();
         Config.loadConfig();
+        
+        makeJDBCConnection();
+
         
         Spark.setPort(Integer.parseInt(Config.getString("sparkport")));
         
@@ -55,4 +67,24 @@ public class App
 			}
 		});
     }
+    
+	private static void makeJDBCConnection() {
+		 
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+			return;
+		}
+ 
+		try {
+			// DriverManager: The basic service for managing a set of JDBC drivers.
+			crunchifyConn = DriverManager.getConnection("jdbc:mysql://" + Config.getString("mysqlip") + ":" + Config.getString("mysqlport")+
+					"/"+Config.getString("mysqldatabase"), Config.getString("mysqlusername"), Config.getString("mysqlpassword"));
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return;
+		}
+ 
+	}
 }
